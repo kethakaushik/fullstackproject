2feffa52026630a53385e7ac89d5eee910aa705a
Kaushik ketha <83337345+kethakaushik@users.noreply.github.com>
2024-03-18 21:21:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -16,11 +15,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://kaushikketha:kaushikketha@cluster0.aox2hbb.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = '';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-// mongoose.set('useFindAndModify', false);

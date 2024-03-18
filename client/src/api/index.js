718dfc5caaa47334de0302fcd1361cdc14a1268d
Kaushik ketha <83337345+kethakaushik@users.noreply.github.com>
2024-03-18 22:17:52 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
-
-// const API = axios.create({ baseURL: 'http://localhost:5000' });
-const API = axios.create({ baseURL: 'https://fullstackproject-of1u.onrender.com/' });
-
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
@@ -20,6 +17,5 @@ export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const comment = (value, id) => API.post(`/posts/${id}/commentPost`, { value });
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
-
 export const signIn = (formData) => API.post('/user/signin', formData);
 export const signUp = (formData) => API.post('/user/signup', formData);

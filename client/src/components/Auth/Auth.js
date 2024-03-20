df042794ac9ef8ad7965e8cd67d775a33203002d
Kaushik ketha <83337345+kethakaushik@users.noreply.github.com>
2024-03-20 20:17:42 +0530

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -2,75 +2,50 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
-
-// import { GoogleLogin } from 'react-google-login';
 import { GoogleLogin } from '@react-oauth/google';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-
 import jwt_decode from 'jwt-decode';
-
 import Icon from './icon';
 import { signin, signup } from '../../actions/auth';
 import { AUTH } from '../../constants/actionTypes';
 import useStyles from './styles';
 import Input from './Input';
-
 const initialState = { firstName: '', lastName: '', email: '', password: '', confirmPassword: '' };
-
 const SignUp = () => {
   const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(false);
   const dispatch = useDispatch();
   const history = useHistory();
-  // const navigate = useNavigate();
   const classes = useStyles();
-
   const [showPassword, setShowPassword] = useState(false);
   const handleShowPassword = () => setShowPassword(!showPassword);
-
   const switchMode = () => {
     setForm(initialState);
     setIsSignup((prevIsSignup) => !prevIsSignup);
     setShowPassword(false);
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (isSignup) {
       dispatch(signup(form, history));
     } else {
       dispatch(signin(form, history));
     }
-
-    // if (isSignup) {
-    //   dispatch(signup(form, navigate));
-    // } else {
-    //   dispatch(signin(form, navigate));
-    // }
   };
-
   const googleSuccess = async (res) => {
     console.log(res);
     // const result = res?.profileObj;
     const result = jwt_decode(res?.credential);
     const token = res?.tokenId;
-
     try {
       dispatch({ type: AUTH, data: { result, token } });
-
       history.push('/');
-      // navigate('/');
     } catch (error) {
       console.log(error);
     }
   };
-
   const googleError = () => console.log('Google Sign In was unsuccessful. Try again later');
-
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
   return (
     <Container component="main" maxWidth="xs">
       <Paper className={classes.paper} elevation={3}>
@@ -99,7 +74,6 @@ const SignUp = () => {
               <Button className={classes.googleButton} color="primary" fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant="contained">
                 Google Sign In
               </Button>
-              
             )}
             onSuccess={googleSuccess}
             onFailure={googleError}
@@ -118,5 +92,4 @@ const SignUp = () => {
     </Container>
   );
 };
-
 export default SignUp;
